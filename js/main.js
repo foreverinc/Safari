d50a4856b57ca2b1b1b7b9e0f8a36cb79cc60ecf
Forever Inc dev <foreverinc.dev@gmail.com>
2023-04-29 13:14:56 +0300

    save

--- js/main.js
+++ js/main.js
@@ -68,3 +68,16 @@
     
 })(jQuery);
 
+var swiper = new swiper(".slide-content", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
